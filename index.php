a19d171b3c2e9ea5865f1e04d57b65b85a877658
Ricc <ricochristian707@gmail.com>
2025-10-21 00:06:32 +0800

    index kelar lanjut

--- index.php
+++ index.php
@@ -22,67 +22,49 @@ if(isset($_SESSION['nama']) && isset($_SESSION['level']) && isset($_SESSION['id'
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>MySchool DB</title>
+   <link rel="stylesheet" href="src/output.css">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
     <!-- Ni link cdn nya ko -->
     <!-- https://tailwindcss.com/docs/installation/play-cdn -->
 
-    <style type="text/tailwindcss">      
-        @theme 
-        {        
-        --color-clifford: #da373d;      
-        }    
-    </style>
-
 </head>
-<body>
-    <main>
+<body class="overflow-hidden ">
         <!-- Ini loginnya -->
-        <section>
-
-            <!-- Kalimat intro -->
-            <h1>Halo, Selamat Datang</h1>
-            <p>Silahkan masukkan data login mu</p>
-
-            <!-- Formnya, mungkin di wrap pake div lagi -->
-             <!-- Klo gagal login gr gr captcha, pake logincap.php -->
-            <form action="login.php" method="post">
-
-                <!-- Nama user -->
-                <p>
-                    <label>Nama:</label><br>
-                    <input type="text" name="nama" required>
-                </p>
-
-                <!-- Password , klo bisa tambahin button show / hide password nya -->
-                <p>
-                    <label>Password:</label><br>
-                    <input type="password" name="password" required>
-                    <button type="button" onclick="">Show</button>
-                </p>
-                
-                <!-- Captcha nya -->
-                <div class="g-recaptcha" data-sitekey="<?php echo $site_key; ?>"></div>
-                <br>
-
-                <!-- Backend nya ada di login.php -->
-                <button type="submit">Masuk</button>
-
-                <div>
-                    <p>Belum punya akun? <a href="register.php">Daftar disini</a></p>
+        <section class="p-20 flex justify-around bg-gradient-to-b from-[#162955] to-[#3059bb] text-white">
+            <img class="z-10 absolute bottom-0 left-0" src="asset/background.png" alt="">
+            <div class=" z-20">
+                <div class="flex flex-col gap-20">
+                    <h1 class="flex gap-2.5"><img class=" max-w-[24px]" src="asset/logo.png" alt="">MySchool DB</h1>
+                    <div class=" border-l-[5px] border-[#3159BB] pl-5">
+                        <h1 class="text-[56px] font-semibold leading-12">Halo, <br>
+                            Selamat Datang</h1>
+                        <p class="text-[#7288BA] text-2xl font-medium">Silahkan masukkan data login mu</p>
+                    </div>
                 </div>
-                <!-- <input type="submit" value="Submit"> -->
-
+            <form class="bg-white text-[#162955] p-8 pr-[68px] max-w-fit mt-10 rounded-xl flex flex-col gap-20" action="login.php"   method="post">
+                <div class="flex flex-col gap-6 ">
+                    <div class="flex flex-col gap-2">
+                        <h1 class="flex gap-2"><img class="max-w-6" src="asset/username.png" alt="">Username</h1>
+                        <input class="text-[#152855]/50 p-3 border border-[#162955] rounded-[10px]" type="text" name="nama" placeholder="*Nama user" required>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <h1 class="flex gap-2"><img class="max-w-6" src="asset/password.png" alt=""> Password:</h1>
+                        <div class="flex gap-1.5">
+                            <input class="w-[400px] text-[#152855]/50 p-3 border border-[#162955] rounded-[10px]" type="password" name="password" placeholder="*Password user" required>
+                            <button class="p-3 border border-[#162955] rounded-[10px] " type="button" onclick=""><img class="max-w-8" src="asset/View.png" alt=""></button>
+                        </div>
+                    </div>
+                    <div class="g-recaptcha" data-sitekey="<?php echo $site_key; ?>"></div>
+                </div>
+                <div class="flex flex-col gap-7">
+                <button class="py-3 px-24 bg-[#3059BB] rounded-xl font-bold text-white" type="submit">Masuk</button>
+                    <h1 class="flex items-center gap-4 text-[#162955] font-semibold text-xl">Tidak punya akun? <a class="text-lg underline text-[#3059BB] font-normal" href="register.php">Daftar sebagai Tamu</a></h1>
+                </div>
             </form>
+        </div>  
+        <img class="max-w-[641px] z-20 border-2 border-[#82A7FF] rounded-xl" src="asset/login-udin.png" alt="">
         </section>
-
-        <!-- Ini imagenya -->
-        <section>
-
-        </section>
-    </main>
 </body>
-</html>
+</html> 
